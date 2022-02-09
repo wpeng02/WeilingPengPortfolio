cd5c601ad227c91713c7a9c0d9948bc037a4152b
wpeng02 <wpeng02@risd.edu>
2022-02-09 12:18:28 -0500

    update0209

--- script.js
+++ script.js
@@ -1,51 +1,93 @@
-$(document).ready(function(){
-    // $(".menu3").children().draggable();
-
-    $("#About").click(function(){
-        $("#About").toggleClass("bold");
-    });
-
+$(document).ready(function(){    
+  
     $("#work").click(function(){
         $("#workmenu").fadeToggle(300);
-        $("#work").toggleClass("bold");
     });
 
+    $("#mobilemenu").click(function(){
+      $("#navmenuopened").slideToggle(300);
+  });
+
+    var text1 = ["Illustrator", "UIUX Designer", "Product Designer"];
     var counter1 = 0;
+    var elem1 = document.getElementById("switch1");
+    var inst = setInterval(change1, 4500);
+    
+    function change1() {
+      elem1.innerHTML = text1[counter1];
+      counter1++;
+      if (counter1 >= text1.length) {
+        counter1 = 0;
+      }
+    }
 
-    $("#switch1").click(function(){
-        if(counter1%3 == 1){
-            $("#switch1").html("<p>Illustrator</p>");
-        }
-        else if(counter1%3 == 2){
-            $("#switch1").html("<p>UIUX Designer</p>");
-        }
-        else{
-            $("#switch1").html("<p>Product Designer</p>");
-        }
-        counter1 = counter1 + 1;
-    });
-
-
+    var text2 = ["Entertaining", "Inspiring", "Suprising"];
     var counter2 = 0;
+    var elem2 = document.getElementById("switch2");
+    var inst = setInterval(change2, 1500);
+    
+    function change2() {
+      elem2.innerHTML = text2[counter2];
+      counter2++;
+      if (counter2 >= text2.length) {
+        counter2 = 0;
+      }
+    }
 
-    $("#switch2").click(function(){
-        if(counter2%3 == 1){
-            $("#switch2").html("<p>Entertaining</p>");
+    var text3 = ["Illustrator", "UIUX Designer", "Product Designer"];
+    var counter3 = 0;
+    var elem3 = document.getElementById("switch3");
+    var inst = setInterval(change3, 4500);
+    
+    function change3() {
+      elem3.innerHTML = text3[counter3];
+      counter3++;
+      if (counter3 >= text3.length) {
+        counter3 = 0;
+      }
+    }
+
+    var text4 = ["Entertaining", "Inspiring", "Suprising"];
+    var counter4 = 0;
+    var elem4 = document.getElementById("switch4");
+    var inst = setInterval(change4, 1500);
+    
+    function change4() {
+      elem4.innerHTML = text4[counter4];
+      counter4++;
+      if (counter4 >= text4.length) {
+        counter4 = 0;
+      }
+    }
+
+    $("#box1").flip();
+
+    var balls = document.getElementsByClassName("ball");
+    document.onmousemove = function(){
+        var x = event.clientX * 100 / window.innerWidth + "%";
+        var y = event.clientY * 100 / window.innerHeight + "%";
+
+        for(var i=0; i<2; i++){
+            balls[i].style.left = x;
+            balls[i].style.top = y;
+            balls[i].style.transform = "translate(-"+x+",-"+y+")";
         }
-        else if(counter2%3 == 2){
-            $("#switch2").html("<p>Inspiring</p>");
-        }
-        else{
-            $("#switch2").html("<p>Suprising</p>");
-        }
-        counter2 = counter2 + 1;
-    });
+    }
+
+    anime({
+        targets: '#wplinedrawing path',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInSine',
+        duration: 1000,
+        direction: 'alternate',
+        loop: false,
+      });
 
 
     var i=0
     setInterval(function(){
         i++;
-     $("img").attr("src", "Image/pic"+i+".jpg");
+     $("#pi").attr("src", "Image/pic"+i+".jpg");
     if(i==1){
         $("#ProjectName").html("<p>Smart Dorm: An Energy-saving Automation Dorm System</p>")
     }
@@ -58,27 +100,59 @@ $(document).ready(function(){
      }
     },5000);
 
-    $("#menuitem2").click(function(){
-        //console.log("click2")
-        $("#container1").text("this is a text method");
-    })
+    $("#Uncanny").click(function(){
+      $("#UncannySeries").css({'display':'block'});
+      $(".close").click(function(){
+        $("#UncannySeries").css({'display':'none'});
+      });
+    });
 
-    $("#menuitem3").click(function(){
-        $("#container1").append("add on something");
-    })
+    $("#Icant").click(function(){
+      $("#IcantSeries").css({'display':'block'});
+      $(".close").click(function(){
+        $("#IcantSeries").css({'display':'none'});
+      });
+    });
 
-    $(".menu").children().hover(function(){
-        //console.log("children")
-        $(this).toggleClass("menuitems")
-        $(this).toggleClass("menuitems2")
-        //$(this) select the thing clicking on
-        //$("#container2").slideToggle();
-        //$("#container2").fadeToggle();
-        // $("#container2").animate({
-        //   opacity: '0.2',
-        //   height: '60%',
-        //   width: '60%',},2000,function(){
-        //     alert("The paragraph is now hidden");
-        // });
-    })
+    $("#Apples").click(function(){
+      $("#ApplesSeries").css({'display':'block'});
+      $(".close").click(function(){
+        $("#ApplesSeries").css({'display':'none'});
+      });
+    });
+
+    $("#LeMooz").click(function(){
+      $("#LeMoozseries").css({'display':'block'});
+      $(".close").click(function(){
+        $("#LeMoozseries").css({'display':'none'});
+      });
+    });
+
+    $("#Stoolpigeon").click(function(){
+      $("#Stoolpigeonseries").css({'display':'block'});
+      $(".close").click(function(){
+        $("#Stoolpigeonseries").css({'display':'none'});
+      });
+    });
+
+    var modal = document.getElementById('modalsingular');
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function(){ 
+      modal.style.display = "none";
+    }
+    var images = document.getElementsByClassName('heroimg1');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    var Title = document.getElementById("titlesingular");
+    var i;
+    for (i = 0; i < images.length; i++) {
+    images[i].onclick = function(){
+       modal.style.display = "block";
+       modalImg.src = this.src;
+       modalImg.alt = this.alt;
+       captionText.innerHTML = this.nextElementSibling.innerHTML;
+       Title.innerHTML = this.parentElement.nextElementSibling.innerHTML;
+   }
+}
+
 });
